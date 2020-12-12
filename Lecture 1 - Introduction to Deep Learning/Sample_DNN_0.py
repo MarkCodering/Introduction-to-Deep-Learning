341dbaac5424b2592e779ad0bcc598fdf6a12d7e
MarkCodering <58692435+MarkCodering@users.noreply.github.com>
2020-12-12 20:28:02 +0800

    code modification

--- Lecture 1 - Introduction to Deep Learning/Sample_DNN_0.py
+++ Lecture 1 - Introduction to Deep Learning/Sample_DNN_0.py
@@ -1,7 +1,9 @@
 weight = 0.5
 
+#Input Data
 input_value = int(input())
 
+#Nerual Network (Single Neural)
 def nerual_network(input_value, weight):
     prediction = input_value * weight
     return prediction
